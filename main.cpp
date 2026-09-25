1ed4ff9c1425b8dee1e9bb35927fc236de55b37a
davidgalindo-git <jdavidgalindos@gmail.com>
2026-09-25 13:24:58 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/**
+ * Un robot (robot) doit récupérer un objet (objet) sur un terrain rocheux adjacent à une route.
+ * Le robot peut se déplacer à une vitesse plus rapide sur la route que sur le terrain rocheux,
+ * il voudra donc le faire sur une certaine distance avant de se déplacer en ligne droite vers l'objet.
+ * Cette fonction sert à calculer le temps total nécessaire au robot pour atteindre son objectif.
+ * @return
+ */
+
+
+int main() {
+
+
+}
